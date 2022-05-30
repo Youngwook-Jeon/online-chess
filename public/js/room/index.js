74d71e122cb46bb00c8205684c4f174a54825e22
Young wook Jeon <ela87@naver.com>
2022-05-30 18:23:37 +0900

    Display the chess pieces

--- public/js/room/index.js
+++ public/js/room/index.js
@@ -1,10 +1,28 @@
 // ================
+// DOM Elements
+// ================
+const room = document.getElementById('game-room');
+const boxes = document.querySelectorAll('.box');
+const playerLight = document.getElementById('player-light');
+const playerBlack = document.getElementById('player-black');
+const waitingMessage = document.getElementById('waiting-message');
+const playerLightTimer = playerLight.querySelector('.timer');
+const playerBlackTimer = playerBlack.querySelector('.timer');
+const lightCapturedPieces = document.getElementById('light-captured-pieces');
+const blackCapturedPieces = document.getElementById('black-captured-pieces');
+// const piecesToPromoteContainer = document.getElementById("pieces-to-promote-container");
+// const piecesToPromote = document.getElementById("pieces-to-promote");
+// const gameOverMessageContainer = document.getElementById("game-over-message-container");
+// const winnerUsername = gameOverMessageContainer.querySelector("p strong");
+// const myScoreElement = document.getElementById("my-score");
+// const enemyScoreElement = document.getElementById("enemy-score");
+
+// ================
 // Game Variables
 // ================
-
 let user = null;
 
-let search = window.location.split("&");
+let search = window.location.search.split('&');
 
 let roomId = null;
 let password = null;
@@ -25,10 +43,10 @@ let enemyScore = 0;
 let gameStartedAtTimestamp = null;
 
 if (search.length > 1) {
-  roomId = search[0].split("=")[1];
-  password = search[1].split("=")[1];
+  roomId = search[0].split('=')[1];
+  password = search[1].split('=')[1];
 } else {
-  roomId = search[0].split("=")[1];
+  roomId = search[0].split('=')[1];
 }
 
 // ================
@@ -39,12 +57,40 @@ const fetchUserCallback = (data) => {
   user = data;
 
   if (password) {
-    socket.emit("user-connected", user, roomId, password);
+    socket.emit('user-connected', user, roomId, password);
   } else {
-    socket.emit("user-connected", user, roomId);
+    socket.emit('user-connected', user, roomId);
   }
 
-  socket.emit("get-game-details", roomId, user);
-}
+  socket.emit('get-game-details', roomId, user);
+};
 
-fetchData("/api/user-info", fetchUserCallback);
+fetchData('/api/user-info', fetchUserCallback);
+
+const displayChessPieces = () => {
+  boxes.forEach((box) => {
+    box.innerHTML = '';
+  });
+
+  lightPieces.forEach((piece) => {
+    let box = document.getElementById(piece.position);
+
+    box.innerHTML += `
+      <div class="piece light" data-piece="${piece.piece}" data-points="${piece.points}">
+        <img src="${piece.icon}" alt="Chess Piece">
+      </div>
+    `;
+  });
+
+  blackPieces.forEach((piece) => {
+    let box = document.getElementById(piece.position);
+
+    box.innerHTML += `
+      <div class="piece black" data-piece="${piece.piece}" data-points="${piece.points}">
+        <img src="${piece.icon}" alt="Chess Piece">
+      </div>
+    `;
+  });
+};
+
+displayChessPieces();
